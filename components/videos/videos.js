32efa4d592a39b0982080bf5f55e5c655aa32dda
Anish Suwal <anishbhaktasuwal@gmail.com>
2023-12-01 17:12:54 +0545

    update youtube

--- components/videos/videos.js
+++ components/videos/videos.js
@@ -1,122 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import YouTube from 'react-player/youtube';
+
+const videoData = [
+  { url: 'https://www.youtube.com/watch?v=P27waC05Hdk&t=160s', title: 'How in vitro fertilization (IVF) works' },
+  { url: 'https://www.youtube.com/watch?v=v1M5ZqTIeTw', title: 'IVF Process Step by Step ...' },
+  { url: 'https://www.youtube.com/watch?v=GeigYib39Rs', title: '3D animation of how IVF works' },
+  { url: 'https://www.youtube.com/watch?v=3jkxdfj5PeY', title: '48-Year-Old Woman Delivers a Baby' },
+  { url: 'https://www.youtube.com/watch?v=bslSF89n-2M', title: 'Best IVF services in Nepal' },
+  { url: 'https://www.youtube.com/watch?v=flOCcHVfWw4', title: ' Tests And Treatment For Infertility' },
+  { url: 'https://www.youtube.com/watch?v=LZHWpM5dJIE', title: 'Pregnancy after Hysteroscopic resection' },
+  { url: 'https://www.youtube.com/watch?v=4esJ5w8l6mk', title: 'Triple pregnancy in IVF with 1 spontaneous abortion and ...' },
+  { url: 'https://www.youtube.com/watch?v=ntGkwMb4Wis', title: 'Can white vaginal discharge cause infertility?' },
+
+  { url: 'https://www.youtube.com/watch?v=8KLcUegpYQ8', title: 'Pregnancy with pv bleeding. Threatened abortion' },
+
+  { url: 'https://www.youtube.com/watch?v=3Xvjh_FM5F0', title: 'Ovarian hyper stimulation syndrome in ART' },
+
+  { url: 'https://www.youtube.com/watch?v=DJj360nFNOs', title: 'Misconception of public that IVF means pregnancy with donor ' },
 
 
 
-import { useEffect, useState } from 'react';
-import ReactPlayer from 'react-player/youtube'
-export default function Videos() {
 
+  // Add other video URLs and titles here
+];
+
+const VideoCard = ({ url, title, hasWindow }) => (
+  <div className='border-2 p-2 bg-green-50'>
+    {hasWindow && <YouTube url={url} width={'100%'} height={'200px'} />}
+    <div className='mt-5'>
+      <a href={url} target="_blank" rel="noopener noreferrer" className='text-sm font-semibold'>
+        {title}
+      </a>
+    </div>
+  </div>
+);
+
+const Videos = () => {
   const [hasWindow, setHasWindow] = useState(false);
+
   useEffect(() => {
-    if (typeof window !== "undefined") {
+    if (typeof window !== 'undefined') {
       setHasWindow(true);
     }
   }, []);
 
-
   return (
     <section id="videos" className="scroll-my-[130px] md:scroll-my-[200px]">
-      <div className="text-center mt-10 m-5  xl:m-32 ">
+      {/* ... Other content ... */}
+      <div className="text-center mt-5 m-5  xl:m-32 ">
         <div className="text-center  ">
           <p className="text-xl md:text-2xl font-bold text-green-600"> Videos</p>
         </div>
-        <div className="grid grid-row-1 md:grid-cols-2 lg:grid-cols-4 justify-center items-center 
-        gap-5 p-2 mt-5 text-bold  shadow-2xl bg-white rounded-lg">
-          <div className='border-2 p-2 bg-green-50'>
-            {
-              hasWindow && <ReactPlayer url='https://www.youtube.com/watch?v=P27waC05Hdk&t=160s' width={'100%'} height={'200px'} />
-
-            }
-            <div className='mt-5'>
-              <a href="https://www.youtube.com/watch?v=P27waC05Hdk&t=160s" target="_blank" rel="noreferrer" className=' text-sm font-semibold '>
-                How in vitro fertilization (IVF) works</a>
-            </div>
-          </div>
-
-          <div className='border-2 p-2 bg-green-50'>
-            {
-              hasWindow && <ReactPlayer url='https://www.youtube.com/watch?v=v1M5ZqTIeTw' width={'100%'} height={'200px'} />
-
-            }
-            <div className='mt-5'>
-              <a href="https://www.youtube.com/watch?v=v1M5ZqTIeTw" target="_blank" rel="noreferrer" className=' text-sm font-semibold '>
-                IVF Process Step by Step ...</a>
-            </div>
-          </div>
-          <div className='border-2 p-2 bg-green-50'>
-            {
-              hasWindow && <ReactPlayer url='https://www.youtube.com/watch?v=GeigYib39Rs' width={'100%'} height={'200px'} />
-
-            }
-            <div className='mt-5'>
-              <a href="https://www.youtube.com/watch?v=GeigYib39Rs" target="_blank" rel="noreferrer" className=' text-sm font-semibold '>
-                3D animation of how IVF works
-              </a>
-            </div>
-          </div>
-          <div className='border-2 p-2 bg-green-50'>
-            {
-              hasWindow && <ReactPlayer url='https://www.youtube.com/watch?v=3jkxdfj5PeY' width={'100%'} height={'200px'} />
-
-            }
-            <div className='mt-5'>
-              <a href="https://www.youtube.com/watch?v=3jkxdfj5PeY" target="_blank" rel="noreferrer" className=' text-sm font-semibold '>
-                48-Year-Old Woman Delivers a Baby </a>
-            </div>
-          </div>
-          <div className='border-2 p-2 bg-green-50'>
-            {
-              hasWindow && <ReactPlayer url='https://www.youtube.com/watch?v=bslSF89n-2M' width={'100%'} height={'200px'} />
-
-            }
-            <div className='mt-5'>
-              <a href="https://www.youtube.com/watch?v=bslSF89n-2M" target="_blank" rel="noreferrer" className=' text-sm font-semibold '>
-              Best IVF services in Nepal
-</a>
-            </div>
-          </div>
-          <div className='border-2 p-2 bg-green-50'>
-            {
-              hasWindow && <ReactPlayer url='https://www.youtube.com/watch?v=flOCcHVfWw4' width={'100%'} height={'200px'} />
-
-            }
-            <div className='mt-5'>
-              <a href="https://www.youtube.com/watch?v=flOCcHVfWw4" target="_blank" rel="noreferrer" className=' text-sm font-semibold '>
-              Basics Of Tests And Treatment For Infertility 
-</a>
-            </div>
-          </div>
-          <div className='border-2 p-2 bg-green-50'>
-            {
-              hasWindow && <ReactPlayer url='https://www.youtube.com/watch?v=LZHWpM5dJIE' width={'100%'} height={'200px'} />
-
-            }
-            <div className='mt-5'>
-              <a href="https://www.youtube.com/watch?v=LZHWpM5dJIE" target="_blank" rel="noreferrer" className=' text-sm font-semibold '>
-              Pregnancy after Hysteroscopic resection 
-</a>
-            </div>
-          </div>
-          <div className='border-2 p-2 bg-green-50'>
-            {
-              hasWindow && <ReactPlayer url='https://www.youtube.com/watch?v=o_J10mfXYlI&t=2s' width={'100%'} height={'200px'} />
-
-            }
-            <div className='mt-5'>
-              <a href="https://www.youtube.com/watch?v=o_J10mfXYlI&t=2s" target="_blank" rel="noreferrer" className=' text-sm font-semibold '>
-              ICSI in IVF
-</a>
-            </div>
-            
-          </div>
-                </div>
-                <div className='mt-5  items-center   '>
-              <a href="https://www.youtube.com/@Dr-anupama-suwal-gurung/videos" target="_blank" rel="noreferrer" 
-              className=' text-white text-lg font-bold rounded-lg border p-3 bg-green-500 hover:bg-green-300'>
-            More Videos
-
-                </a>
-            </div> 
+      <div className="grid grid-row-1 md:grid-cols-2 lg:grid-cols-6 justify-center items-center gap-3 text-xs p-2 mt-5 text-bold shadow-2xl bg-white rounded-lg">
+        {videoData.map((video, index) => (
+          <VideoCard key={index} url={video.url} title={video.title} hasWindow={hasWindow} />
+        ))}
+      </div>
+      <div className='mt-5 items-center'>
+        <a href="https://www.youtube.com/@Dr-anupama-suwal-gurung/videos" target="_blank" rel="noopener noreferrer" className='text-white text-lg font-bold rounded-lg border p-3 bg-green-500 hover:bg-green-300'>
+          More Videos
+        </a>
+      </div>
       </div>
     </section>
-  )
-}
+  );
+};
+
+export default Videos;
